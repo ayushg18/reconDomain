dec17937a19fb1a255fbc505c91b7123990d6b32
ayushg18 <87230868+ayushg18@users.noreply.github.com>
2023-08-20 19:55:22 +0530

    Update reconDomain.sh

--- reconDomain.sh
+++ reconDomain.sh
@@ -9,8 +9,8 @@ printf " by @vuln4n6\n\n"
 read -p "Enter the target: " target
 read -p "Target Name: " fileName
 
-subfinder -d $target | tee $fileName-subdomain.txt
-cat $fileName-subdomain.txt | httpx -mc 200 | tee 200-$fileName.txt
-cat $fileName-subdomain.txt | httpx -mc 404 | tee 404-$fileName.txt
-cat $fileName-subdomain.txt | httpx -mc 403 | tee 403-$fileName.txt
-cat $fileName-subdomain.txt | waybackurls | tee $fileName-WaybackUrls.txt
+subfinder -d $target | tee subdomain-$fileName.txt
+cat subdomain-$fileName.txt | httpx -mc 200 | tee 200-$fileName.txt
+cat subdomain-$fileName.txt | httpx -mc 404 | tee 404-$fileName.txt
+cat subdomain-$fileName.txt | httpx -mc 403 | tee 403-$fileName.txt
+cat subdomain-$fileName.txt | waybackurls | tee waybackurls-$fileName.txt
